7c6f706754526996be56ab8994bb609166c9fb2f
goddlts <54337467@qq.com>
2018-09-03 17:25:15 +0800

    用户管理-搜索框

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -6,6 +6,18 @@
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 搜索 -->
+    <el-row class="row">
+      <el-col :span="24">
+        <el-input
+          style="width: 300px"
+          placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -18,5 +30,9 @@ export default {
 <style>
 .card {
   height: 100%;
+}
+.row {
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
 </style>
