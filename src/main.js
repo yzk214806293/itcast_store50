54bc4720ec7e75f6c6c0bad5772f16e8c9f60b4f
goddlts <54337467@qq.com>
2018-09-03 12:03:03 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/index.css';
 
 // 注册插件ElementUI
 Vue.use(ElementUI);
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    login
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="top"
+      label-width="80px">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -11,5 +25,22 @@ export default {
 </script>
 
 <style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.login-form {
+  width: 500px;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: #fff;
+}
+
+.login-btn {
+  width: 100%;
+}
 </style>
