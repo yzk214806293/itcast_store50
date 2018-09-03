a76423e2ee06034c4071ba090b9e71f4128299be
goddlts <54337467@qq.com>
2018-09-03 15:04:37 +0800

    登录功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,61 @@
     <el-form
       class="login-form"
       label-position="top"
-      label-width="80px">
+      label-width="80px"
+      :model="formData">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
-        <el-input></el-input>
+        <el-input v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input type="password"></el-input>
+        <el-input v-model="formData.password" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="login-btn" type="primary">登录</el-button>
+        <el-button @click="handleLogin" class="login-btn" type="primary">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+// 1 绑定文本框 √
+// 2 给按钮注册事件，发送异步请求 √
+// 3 登录成功，跳转/提示/记录token
 export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    // 登录
+    handleLogin() {
+      this.$http
+        .post('login', this.formData)
+        .then((response) => {
+          // 判断登录是否成功
+          // response -> data, status
+          // response.data 服务器返回的数据  data, meta
+          // response.data.meta.status
+          // const status = response.data.meta.status;
+          // const msg = response.data.meta.msg;
 
+          // 获取 response.data中的 msg和status
+          const { meta: { msg, status } } = response.data;
+          if (status === 200) {
+            // 成功
+          } else {
+            // 失败
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
 
