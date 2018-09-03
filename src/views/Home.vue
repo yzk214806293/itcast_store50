82d9ab413193d967089e1b27e15c775045397458
goddlts <54337467@qq.com>
2018-09-03 16:02:05 +0800

    Home的header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="">
+        </el-col>
+        <el-col class="middle" :span="19">
+          <span>电商后台管理系统</span>
+        </el-col>
+        <el-col :span="1">
+          <a class="logout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -15,6 +27,18 @@ export default {
 </script>
 
 <style>
+.logout {
+  text-decoration: none;
+  color: orange;
+}
+
+.middle {
+  text-align: center;
+  line-height: 60px;
+  font-size: 24px;
+  color: #fff;
+}
+
 .el-container {
   height: 100%;
 }
@@ -22,8 +46,9 @@ export default {
 .el-header, .el-footer {
   background-color: #B3C0D1;
   color: #333;
-  text-align: center;
+  /* text-align: center; */
   line-height: 60px;
+  padding: 0;
 }
 
 .el-aside {
