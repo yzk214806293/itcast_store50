41100c5aeb69f2bf3c3f79513d61053ba7646a02
goddlts <54337467@qq.com>
2018-09-03 11:47:41 +0800

    webpack的两个配置规则

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,13 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-// @是src目录，在build的配置文件中配置的
-import Login from '@/views/Login.vue';
+// 1 @是src目录，在build的配置文件中配置的
+//   @  webpack.base.conf 的第38行配置的
+
+// 2 为什么可以省略掉.vue
+// 加载一个模块的时候，如果省略后缀名，默认情况 先加载login.js，如果找不到再加载 login.json
+// 默认的情况可以修改，在webpack.base.conf的35行配置
+import Login from '@/views/Login';
 
 export default new Router({
   routes: [
