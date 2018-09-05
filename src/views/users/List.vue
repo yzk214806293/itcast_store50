d7554a82df945b3e8c0fec10eb86f285d537a8e7
goddlts <54337467@qq.com>
2018-09-05 15:54:48 +0800

    对话框内部的表单

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -107,22 +107,26 @@
 
     <!-- 添加用户的对话框 -->
     <el-dialog title="添加用户" :visible.sync="addUserDialogFormVisible">
-      hello
-      <!-- <el-form :model="form">
-        <el-form-item label="活动名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" auto-complete="off"></el-input>
+      <el-form
+        label-width="80px"
+        :model="formData">
+        <el-form-item label="用户名">
+          <el-input v-model="formData.username" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="活动区域" :label-width="formLabelWidth">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="formData.password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="formData.email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="formData.mobile" auto-complete="off"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-      </div> -->
+        <el-button @click="addUserDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUserDialogFormVisible = false">确 定</el-button>
+      </div>
     </el-dialog>
 
   </el-card>
@@ -145,7 +149,14 @@ export default {
       // 绑定搜索文本框
       searchValue: '',
       // 控制添加用户对话框的显示或隐藏
-      addUserDialogFormVisible: false
+      addUserDialogFormVisible: false,
+      // 绑定的表单对象
+      formData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      }
     };
   },
   created() {
