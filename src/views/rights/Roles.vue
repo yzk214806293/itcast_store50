235543cfad5af474c221dd2a051d8f57626f3c1c
goddlts <54337467@qq.com>
2018-09-06 15:39:25 +0800

    角色列表-未分配权限

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -48,6 +48,10 @@
               </el-row>
             </el-col>
           </el-row>
+          <!-- 未分配权限 -->
+          <el-row v-if="scope.row.children.length === 0">
+            <el-col :span="24">未分配权限</el-col>
+          </el-row>
         </template>
       </el-table-column>
       <el-table-column
