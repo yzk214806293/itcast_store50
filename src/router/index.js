c91845ca3dd11a2a14a3f46b75215c753b8a9528
goddlts <54337467@qq.com>
2018-09-06 10:39:37 +0800

    权限列表-打通路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Router from 'vue-router';
 import Login from '@/views/Login';
 import Home from '@/views/Home';
 import Users from '@/views/users/List';
+import Rights from '@/views/rights/Rights';
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
           name: 'users',
           path: '/users',
           component: Users
+        },
+        {
+          name: 'rights',
+          path: '/rights',
+          component: Rights
         }
       ]
     }
---
+++ src/views/rights/Rights.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card class="card">
+    hello
+  </el-card>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.card {
+  width: 100%;
+}
+</style>
