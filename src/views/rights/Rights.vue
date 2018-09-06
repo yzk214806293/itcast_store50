40732c491e682aa4cb65d7d7a64849ddad839eb3
goddlts <54337467@qq.com>
2018-09-06 10:47:13 +0800

    权限列表-设置表格

--- src/views/rights/Rights.vue
+++ src/views/rights/Rights.vue
@@ -1,17 +1,50 @@
 <template>
   <el-card class="card">
-    hello
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 表格 -->
+    <el-table
+      border
+      stripe
+      :data="tableData"
+      style="width: 100%; margin-top: 10px">
+      <el-table-column
+        type="index"
+        width="60">
+      </el-table-column>
+      <el-table-column
+        prop="authName"
+        label="权限名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="path"
+        label="路径"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="level"
+        label="层级">
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: []
+    }
+  }
 };
 </script>
 
 <style>
 .card {
-  width: 100%;
+  height: 100%;
 }
 </style>
