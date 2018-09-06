d5a742ef6fff8c4564287987980449e1984701f9
goddlts <54337467@qq.com>
2018-09-06 14:53:36 +0800

    角色列表-设置表格

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -1,13 +1,53 @@
 <template>
   <el-card class="card">
     <my-breadcrumb level1="权限管理" level2="角色列表"></my-breadcrumb>
-    
+    <!-- 按钮 -->
+    <el-button style="margin-top: 10px; margin-bottom: 10px">添加角色</el-button>
+    <!-- 表格 -->
+    <el-table
+      border
+      stripe
+      :data="tableData"
+      style="width: 100%">
+      <!-- 展开行 -->
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          hello
+        </template>
+      </el-table-column>
+      <el-table-column
+        type="index"
+        width="60">
+      </el-table-column>
+      <el-table-column
+        prop="roleName"
+        label="角色名称"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="roleDesc"
+        label="角色描述"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" type="primary" icon="el-icon-edit" plain></el-button>
+          <el-button size="mini" type="danger" icon="el-icon-delete" plain></el-button>
+          <el-button size="mini" type="success" icon="el-icon-check" plain></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: []
+    };
+  }
 };
 </script>
 
