ac7590054d18823d5347ffd40551f290769c1889
goddlts <54337467@qq.com>
2018-09-08 11:36:07 +0800

    商品分类-展示基础表格

--- src/views/goods/Categories.vue
+++ src/views/goods/Categories.vue
@@ -16,13 +16,20 @@
         width="300">
       </el-table-column>
       <el-table-column
-        prop="cat_level"
         label="级别"
         width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-else-if="scope.row.cat_level === 1">二级</span>
+          <span v-else-if="scope.row.cat_level === 2">三级</span>
+        </template>
       </el-table-column>
       <el-table-column
         prop="cat_deleted"
         label="是否有效">
+        <template slot-scope="scope">
+          {{ scope.row.cat_deleted ? '无效' : '有效' }}
+        </template>
       </el-table-column>
       <el-table-column
         label="操作">
@@ -41,6 +48,22 @@ export default {
     return {
       tableData: []
     };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    // 加载商品分类数据
+    async loadData() {
+      const response = await this.$http.get('categories?type=3&pagenum=1&pagesize=5');
+
+      const { meta: { msg, status } } = response.data;
+      if (status === 200) {
+        this.tableData = response.data.data.result;
+      } else {
+        this.$message.error(msg);
+      }
+    }
   }
 };
 </script>
